beeb481766cb60391c3f293eef66e0388b1926f5
urdad <70736338+urdadx@users.noreply.github.com>
2023-06-05 10:45:00 +0000

    added navigation menu

--- mobile/app/index.js
+++ mobile/app/index.js
@@ -1,14 +1,12 @@
-import { Text, View } from "react-native";
+import MainContainer from "./navigation/MainContainer";
+
 
 const Page = () => {
   return (
-    <View className = "bg-red-300 h-full flex items-center justify-center">
-      <View>
-        <Text>Yoooo React native app router</Text>
-        <Text>This is the first page of your app.</Text>
-      </View>
-    </View>
+        <MainContainer />
   );
 }
 
 export default Page
+
+
